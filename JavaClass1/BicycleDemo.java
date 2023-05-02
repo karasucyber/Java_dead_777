e932181ac60277324c673aec1b5781e422c7e0f0
Aquillae777 <Aquillae777@gmail.com>
2023-05-02 15:21:07 -0300

    01

--- JavaClass1/BicycleDemo.java
+++ JavaClass1/BicycleDemo.java
@@ -1,10 +1,10 @@
-class BicycleDemo{
-	public static void main(String[] args){
-	Bicycle	Bike1 = new Bicycle();
+class BicycleDemo {
+	public static void main(String[] args) {
+		Bicycle Bike1 = new Bicycle();
 
-	 bike1.changeCadence(50);
-        bike1.speedUp(10);
-        bike1.changeGear(2);
-          bike1.changeCadence(50);
-       }
+		bike1.changeCadence(50);
+		bike1.speedUp(10);
+		bike1.changeGear(2);
+		bike1.changeCadence(50);
+	}
 }
