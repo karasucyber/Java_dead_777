8bdbbc5a420383fc0221d16d4d5b77e62e145fb3
Aquillae777 <Aquillae777@gmail.com>
2023-05-04 12:51:40 -0300

    Play

--- JavaClass3/Escola.java
+++ JavaClass3/Escola.java
@@ -1,29 +1,25 @@
 class Escola {
-	//Atributos
-	private String nome, CNPJ;   
+	// Atributos
+	private String nome, CNPJ;
 	private Endereco endereco;
-	private Departamento departamentos [];
-	private Aluno discentes [];
-	private int nr_discentes , nr_departamentos;
-   
-	//Métodos
-	public Escola ( String nome , String CNPJ) {
+	private Departamento departamentos[];
+	private Aluno discentes[];
+	private int nr_discentes, nr_departamentos;
+
+	// Métodos
+	public Escola(String nome, String CNPJ) {
 		this.nome = nome;
-        this.CNPJ = CNPJ;
-		this.departamentos = new Departamento [10];
-        this.discentes = new Aluno [1000];
+		this.CNPJ = CNPJ;
+		this.departamentos = new Departamento[10];
+		this.discentes = new Aluno[1000];
 		this.nr_departamentos = 0;
-	    this.nr_discentes = 0;
-    }
-    public void criarDepartamento ( String nomeDepartamento ) {
-        if ( nr_departamentos <= 10 )
-        {
-			departamentos [ nr_departamentos ] = new Departamento ( nomeDepartamento );                
-			nr_departamentos++;
-        } else {
-			System.out.println ( "Nao e possivel criar outro Departamento." );
-       }
-    public void matricularAluno ( Aluno novoAluno ) {
-       discentes [ nr_discentes ] = novoAluno;
-    }
+		this.nr_discentes = 0;
+	}
+
+	public void criarDepartamento ( String nomeDepartamento ) {if ( nr_departamentos <= 10 ){departamentos [ nr_departamentos ] = new Departamento ( nomeDepartamento );nr_departamentos++ } else {
+   System.out.println ( "Nao e possivel criar outro Departamento." ); }
+
+	public void matricularAluno(Aluno novoAluno) {
+		discentes[nr_discentes] = novoAluno;
+	}
 }
