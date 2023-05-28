5ede47e4a9ff246e369540e2cd4bdda05917ee9b
Aquillae777 <Aquillae777@gmail.com>
2023-05-28 17:33:13 -0300

    String

---
+++ Aquillae/Tres.java
@@ -0,0 +1,14 @@
+package Aquillae;
+
+public class Tres{
+    public static void main(String[] args) {
+        String nome = "Kauã";
+        int idade = 19;
+        String data = "10/20";
+        String endereço = "rua" ;
+        String text = "meu nome é " + nome + " morando em " + endereço + " e tenho " + idade + " anos" + " nasci na data " + data;
+        
+        System.out.println(text);
+    }
+    
+}
