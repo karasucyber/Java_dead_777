e2ea47b085ab40f101b479ff82155c026c860dfc
Aquillae777 <Aquillae777@gmail.com>
2023-05-28 17:33:01 -0300

    sout

---
+++ Aquillae/Dois.java
@@ -0,0 +1,50 @@
+package Aquillae;
+
+public class Dois {
+    public static void main(String[] args) {
+    /* Tipos primitivos e seus bytes */
+    /*int, double, float, char,byte, short, long, boolean */
+    
+    int Tipo1 = 4; 
+    double Tipo2 = 8;
+    byte Tipo3 = 1;
+    short Tipo4 = 2;
+    boolean Tipo5= false;
+    char Tipo6 = '2'; 
+    String Aquillae = "Teste dos tipos primitivos e suas reservas de memoria";
+    var nome2 = "Obrigado";
+
+ /*  float Force = (Float) 200.0; 
+  * forçando a memoria 
+ */
+ 
+    System.out.println("Int possui" + Tipo1 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+    System.out.println("double possui" + Tipo2 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+
+    System.out.println("byte possui" + Tipo3 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+
+    System.out.println("short possui" + Tipo4 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+
+    System.out.println("boolean possui" + Tipo5 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+
+    System.out.println("chart possui" + Tipo6 + "bytes"  + "\n double possui !!!"  ) ;
+    System.out.println("******************************");
+
+    System.out.println(Aquillae);
+    System.out.println(nome2);
+}}
+
+
+
+
+
+
+/*
+ * comando psvm + enter, cria a estrutura main necessária 
+ * comando sout + enter,cria System.out.printl
+ */
