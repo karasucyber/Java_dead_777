28118457711ade6ac93d80a4a95dee8a6279d219
Aquillae777 <Aquillae777@gmail.com>
2023-05-28 17:32:44 -0300

    psmv

---
+++ Aquillae/Zero.java
@@ -0,0 +1,13 @@
+package Aquillae;
+
+public class Zero {
+    public static void main(String args){
+        System.out.println("Olá main");
+    }
+}
+
+/*
+ * Erros cometidos faltou [] 
+ * erro apresentado = não foi possível encontrar main.
+ * o correto String[].
+ */
