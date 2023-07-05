f2d1925fa0477cc436ada9b3983ef44ee895f307
Aquillae777 <Aquillae777@gmail.com>
2023-07-04 21:29:51 -0300

    up

---
+++ Aquillae/十二.java
@@ -0,0 +1,26 @@
+package Aquillae;
+
+public class 十二 {
+ public static void main(String[] args) {
+    double valorTotal = 30000;
+
+    for(int Parcela = 1 ; Parcela <= valorTotal; Parcela++){
+
+        double valorParcela =  valorTotal / Parcela ; 
+
+        if(valorParcela <= 1000){
+            
+
+            break;
+
+        }else{
+            continue;
+        }
+    }
+    
+    
+
+    
+    System.out.println();
+ }    
+}
---
+++ Aquillae/十四.java
@@ -0,0 +1,18 @@
+package Aquillae;
+
+public class 十四 {
+    public static void main(String[] args) {
+        String [] nomes = new String[3];
+        
+        nomes[0] = "Naruto" ;
+        nomes[1] =  "Goku" ;
+        nomes[2] =  "Saitama" ;
+        
+        for(int i =  0; i < nomes.length; i ++){
+         System.out.println(nomes[i]); 
+
+        }
+
+    }
+    
+}
