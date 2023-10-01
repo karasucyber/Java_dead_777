164c815011d23f7648a8df2a6f36e64a4cd52db2
aquillae7 <aquillae777@gmail.com>
2023-10-01 09:28:17 -0300

    if simples

--- Aquillae/五.java
+++ Aquillae/五.java
@@ -3,23 +3,22 @@ package Aquillae;
 public class 五 {
 
     public static void main(String[] args) {
-        int idade = 15;
-        int idade2 = 17;
-        boolean autorizacao = idade >= 19;
+     int salario = 1000;
+     int compra = 100;
 
-        if(autorizacao){
-
-            System.out.println("True");
-        }else{
-            System.out.println("deu false");
-        }
-        if(!autorizacao) {
-            System.out.println("falso");
-        }
-
-
-
+     if(compra > salario){
+        System.out.println("Você não pode comprar");
+     }else{
+        System.out.println("pode comprar");
     }
-    
- 
+
+
+    if(compra < salario){
+        salario = salario - compra;
+        System.out.println("caso compre sobrará:" + salario);
+    }else{
+        salario = salario - compra;
+        System.out.println("caso compre seu saldo ficará negativo:" + salario );
+    }
+}
 }
